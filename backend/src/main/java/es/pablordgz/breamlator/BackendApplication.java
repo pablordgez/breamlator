8218ffd55dc7d70d72129d2a0ed051d085770df1
Pablo Rodríguez <85810125+pablordgez@users.noreply.github.com>
2025-03-07 21:36:09 +0100

    Fixed pom dependency and removed leftover line from testing

--- backend/src/main/java/es/pablordgz/breamlator/BackendApplication.java
+++ backend/src/main/java/es/pablordgz/breamlator/BackendApplication.java
@@ -1,6 +1,5 @@
 package es.pablordgz.breamlator;
 
-import es.pablordgz.breamlator.repositories.AI.GeminiAIRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ import java.io.IOException;
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) throws IOException {
-        GeminiAIRepository geminiAIRepository = new GeminiAIRepository();
         SpringApplication.run(BackendApplication.class, args);
     }
 
