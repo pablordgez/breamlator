7f2a196b349b36f305b105e0ce0fd8b28a75d867
Pablo Rodríguez <85810125+pablordgez@users.noreply.github.com>
2025-03-08 03:23:49 +0100

    Added additional "languages"

--- backend/src/main/java/es/pablordgz/breamlator/repositories/Language/LanguageRepositoryImpl.java
+++ backend/src/main/java/es/pablordgz/breamlator/repositories/Language/LanguageRepositoryImpl.java
@@ -19,13 +19,20 @@ public class LanguageRepositoryImpl implements LanguageRepository {
     public void init() {
         languages = new ArrayList<>();
         TranslationService translationService = new AITranslationService(new GeminiAIRepository());
-        languages.add(new Language("Old English", "Old English", translationService));
+        languages.add(new Language("Old English", "Shakespeare English", translationService));
         languages.add(new Language("Pirate", "Talk like a pirate", translationService));
         languages.add(new Language("Poetry", "Talk using rhymes", translationService));
         languages.add(new Language("Jar Jar Binks", "Talk like Jar Jar Binks", translationService));
         languages.add(new Language("Yoda", "Talk like Yoda", translationService));
         languages.add(new Language("Gollum", "Talk like Gollum", translationService));
         languages.add(new Language("Anime girl", "Talk like an anime girl", translationService));
+        languages.add(new Language("Secret", "Talk like you are trying very hard to keep a secret", translationService));
+        languages.add(new Language("Paranoid", "Talk like you are paranoid about secret agencies investigating you", translationService));
+        languages.add(new Language("Chicote", "Talk like Alberto Chicote in the Spanish TV Show Pesadilla en la Cocina", translationService));
+        languages.add(new Language("Rajoy", "Talk like Spanish politician Mariano Rajoy", translationService));
+        languages.add(new Language("Rapper", "Talk like a rapper", translationService));
+        languages.add(new Language("Rajoy v2", "Talk like Spanish politician Mariano Rajoy. Make sure to use some messy and non sensical sentences like he does", translationService));
+        languages.add(new Language("Teletienda", "Talk like you are a salesman on a TV home shopping network", translationService));
     }
 
     @Override
